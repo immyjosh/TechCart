070d49a661affd9f038d220d2d8b30ed54b17853
immyjosh <immanueljoshuapaul@gmail.com>
2018-02-10 13:43:38 +0530

    Added Files

---
+++ TechCart/TechCartPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace TechCart
+{
+    public partial class TechCartPage : ContentPage
+    {
+        public TechCartPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
